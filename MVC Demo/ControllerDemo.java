bf1c331224b3274698f3c27850598ffca9151035
GPAVANSRIVATSA <107201082+GPAVANSRIVATSA@users.noreply.github.com>
2024-03-14 16:56:06 +0530

    Add files via upload

---
+++ MVC Demo/ControllerDemo.java
@@ -0,0 +1,16 @@
+package com.Thymeleaf.ThymeleafDemo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Date;
+
+@Controller
+public class ControllerDemo {
+
+    @GetMapping("/hello")
+    public String sayHello(Model model) {
+        model.addAttribute("theDate", new Date());
+        return "helloWorld";
+    }
+}
