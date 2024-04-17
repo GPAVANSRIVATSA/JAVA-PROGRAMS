6113e86b18d4c3dc2cc18e21b6dc67b343342ce4
GPAVANSRIVATSA <107201082+GPAVANSRIVATSA@users.noreply.github.com>
2024-04-17 16:49:43 +0530

    Delete ToDoList directory

--- ToDoList/ThingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.List.ToDoListDemo.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.List.ToDoListDemo.Things.ToDoThings;
-
-public interface ThingRepository extends JpaRepository<ToDoThings,Integer>{
-
-}
